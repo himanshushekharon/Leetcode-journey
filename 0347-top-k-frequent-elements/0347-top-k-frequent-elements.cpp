a9ceefedb1b81e2dd8f2fa0d607a6812be114a97
Himanshu Shekhar <148798610+himanshushekharon@users.noreply.github.com>
2026-03-17 18:20:23 +0530

    Time: 4 ms (34.59%), Space: 17.8 MB (84.49%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -5,18 +5,18 @@ public:
         for(int i:nums){
             mp[i]++;
         }
-       vector<int>unique;
+       vector<int>vt;
        for(auto [a,b]:mp){
-        unique.push_back(a);
+        vt.push_back(a);
        }
-       sort(unique.begin(),unique.end(),[&](int a,int b){
+       sort(vt.begin(),vt.end(),[&](int a,int b){
         if(mp[a]==mp[b]){
             return a<b;
         }
         return mp[a]>mp[b];
        });
         vector<int>v;
-        for(int i:unique){
+        for(int i:vt){
             v.push_back(i);
             k--;
             if(k==0){
