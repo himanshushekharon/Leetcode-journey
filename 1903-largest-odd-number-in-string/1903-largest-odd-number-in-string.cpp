65602cb602eb41ff9ccf86547dd1ab136d8bc633
Himanshu Shekhar <148798610+himanshushekharon@users.noreply.github.com>
2026-04-16 22:52:44 +0530

    Time: 4 ms (22.44%), Space: 16.7 MB (98.18%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -1,27 +1,35 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        vector<char>s;
+        // vector<char>s;
         
-        for(char c:num){
-            s.push_back(c);
-        }
-        string str="";
-        int j=0;
-        int n=s.size();
-        for(int i=n-1;i>=0;i--){
-            int a=s[i]-'0';
-            if(a%2!=0){
-                str+=s[i];
-                j=i;
+        // for(char c:num){
+        //     s.push_back(c);
+        // }
+        // string str="";
+        // int j=0;
+        // int n=s.size();
+        // for(int i=n-1;i>=0;i--){
+        //     int a=s[i]-'0';
+        //     if(a%2!=0){
+        //         str+=s[i];
+        //         j=i;
+        //         break;
+        //     }
+        // }
+        // for(int k=j-1;k>=0;k--){
+        //     str+=s[k];
+        // }
+        // reverse(str.begin(),str.end());
+        // return str;
+        for(int i=num.size()-1;i>=0;i--){
+            int n=num[i]-'0';
+            if(n%2!=0){
                 break;
+            }else{
+                num.pop_back();
             }
         }
-        for(int k=j-1;k>=0;k--){
-            str+=s[k];
-        }
-        reverse(str.begin(),str.end());
-        return str;
-
+        return num;
     }
 };
