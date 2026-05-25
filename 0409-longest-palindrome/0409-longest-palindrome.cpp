d89b8afc206133c828f849ba8cb9bf7c3f178ed1
Himanshu Shekhar <148798610+himanshushekharon@users.noreply.github.com>
2026-05-25 14:21:06 +0530

    Time: 1 ms (26.67%), Space: 9.3 MB (40.21%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int>mp;
+        for(char c:s){
+            mp[c]++;
+        }
+        int ans=0;
+        bool odd=false;
+        for(auto [a,b]:mp){
+            if(b%2==0){
+                ans+=b;
+            }else{
+                ans+=b-1;
+                odd=true;
+            }
+        }
+        if(odd) ans++;
+        return ans;
+    }
+};
