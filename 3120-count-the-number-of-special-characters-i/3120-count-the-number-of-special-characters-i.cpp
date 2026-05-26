7fe7003e3d7917fd7f61da8807bb220302f25c78
Himanshu Shekhar <148798610+himanshushekharon@users.noreply.github.com>
2026-05-26 21:15:51 +0530

    Time: 4 ms (13.46%), Space: 11.3 MB (5.34%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        set<int>s;
-        for(char i:word){
-            s.insert(int(i));
+        // set<int>s;
+        // for(char i:word){
+        //     s.insert(int(i));
+        // }
+        // vector<int>v;
+        // for(int i:s){
+        //     v.push_back(i);
+        //     cout<< i<<" ";
+        // }
+        // int cnt=0;
+        // for(int i=0;i<v.size();i++){
+        //     for(int j=i;j<v.size();j++){
+        //         if(abs(v[i]-v[j])==32) cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        map<char,int>mp;
+        for(char c:word){
+            mp[c]++;
         }
-        vector<int>v;
-        for(int i:s){
-            v.push_back(i);
-            cout<< i<<" ";
-        }
+
         int cnt=0;
-        for(int i=0;i<v.size();i++){
-            for(int j=i;j<v.size();j++){
-                if(abs(v[i]-v[j])==32) cnt++;
+        for(char c='a'; c<='z';++c){
+            if(mp[c]>=1 && mp[toupper(c)]>=1){
+                cnt++;
             }
         }
         return cnt;
