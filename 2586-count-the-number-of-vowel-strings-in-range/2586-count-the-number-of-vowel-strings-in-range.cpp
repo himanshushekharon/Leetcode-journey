c3385d005f1da49d667a9532b4fb02329e03c5fd
Himanshu Shekhar <148798610+himanshushekharon@users.noreply.github.com>
2026-07-02 10:33:14 +0530

    Time: 0 ms (100%), Space: 37 MB (12.57%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.cpp
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int vowelStrings(vector<string>& strs, int left, int right) {
         int cnt=0;
+        string v = "aeiou";
         for(int i=left;i<=right;i++){
             string str=strs[i];
             int l=strs[i].length();
-            if((str[0]=='a'||str[0]=='e'||str[0]== 'i'||str[0]== 'o'|| str[0]=='u') && (str[l-1]=='a'||str[l-1]=='e'||str[l-1]== 'i'||str[l-1]== 'o'|| str[l-1]=='u')) cnt++;
-        }
+            if( (v.find(str[0])!= string::npos)  && (v.find(str[l-1])!= string::npos) ) cnt++;
+        }              //if (str.find(...) != string::npos), you are explicitly asking: "Is the result different from 'not found'?"
         return cnt;
     }
 };
